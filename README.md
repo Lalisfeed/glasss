1acf5f05a530e0ceed67598971a44ffcfd0f1273
Lalisfeed <59479762+Lalisfeed@users.noreply.github.com>
2021-02-02 20:54:18 +0530

    version upgraded

--- README.md
+++ README.md
@@ -1,40 +1,99 @@
 # Glasss
 
+[![npm version](https://img.shields.io/npm/v/glasss.svg?style=flat-square)](https://www.npmjs.org/package/glasss) 
+[![install size](https://packagephobia.com/badge?p=glasss)](https://packagephobia.com/result?p=glasss) 
+[![npm downloads](https://img.shields.io/npm/dt/glasss.svg?style=flat-square)](http://npm-stat.com/charts.html?package=glasss) 
+
 # What is dis?
-### A Glassmorphism based NPM Library 
+
+>  A Glassmorphism based NPM Library 
 
 # Installation
 
 ```
-npm i glasss --save
+npm i glasss
 ```
 
 # How to Use
 
-Add an attribute with  ```class="glasscss"``` to your `html` element 
+* **For Single Class Glassmorphism**
 
-```html
-<div class="glasscss"></div>
-```
+    Add an attribute with  ```class="glasscss"``` to your `html` element 
 
-Then...
+    ```html
+    <div class="glasscss"></div>
+    ```
 
-Add this to your `Javascript` file
+    Then...
 
-```javascript 
-import { glasscss } from 'glasss';
+    Add this to your `Javascript` file
 
-glasscss({
-    back_ground: "rgba( 255, 255, 255, 0.20)", // alpha limit at [0 - 0.25]
-    backdrop_filter: "10px", // filter limit at [0.0 - 20.0]
-    border_radius: "10px", // 10px is nice
-    border_width: true, // true for best resuts
-    color_value: "black", // your color
-    margin_value: "10px", // your value
-    padding_value: "10px", // your value
-});
-```
+    ```javascript 
+    import { glasscss } from 'glasss';
+
+    glasscss({
+        back_ground: "rgba( 255, 255, 255, 0.20)", // alpha limit at [0 - 0.75] recommended
+        backdrop_filter: "10px", // filter limit at [0.0 - 20.0] recommended
+        border_radius: "10px", // 10px is recommended
+        border_wide: true, // true for best results
+        color_value: "black", // your color
+        margin_value: "10px", // your value
+        padding_value: "10px", // your value
+    });
+    ```
+
+* **For Multi-Class Glassmorphism**
+
+    Add AMAP classes to your `html` page 
+    
+    ```html
+    <div class="class1"></div>
+    <div class="class2"></div>
+    <div class="class3"></div>
+    ```
+    
+    Then...
+    
+    Add this to your `Javascript` file
+    
+    ```javascript 
+    import { glassClass } from 'glasss';
+    
+    glassClass({
+        "class1": {
+            back_ground: "rgba( 0, 255 , 255, 0.25)", 
+            backdrop_filter: "5px", 
+            border_radius: "10px", 
+            border_wide: true, 
+            color_value: "white", 
+            margin_value: "0px", 
+            padding_value: "0px"
+        }, 
+        "class2": {
+            back_ground: "rgba( 255, 0, 255, 0.50)", 
+            backdrop_filter: "10px", 
+            border_radius: "10px", 
+            border_wide: true, 
+            color_value: "yellow", 
+            margin_value: "10px", 
+            padding_value: "10px"
+        }, 
+        "class3": {
+            back_ground: "rgba( 255, 255, 0, 0.75)", 
+            backdrop_filter: "15px", 
+            border_radius: "10px", 
+            border_wide: true, 
+            color_value: "black", 
+            margin_value: "20px", 
+            padding_value: "20px"
+        }
+    });
+    ```
 
 # Congratulations
 
-That's All You have added GLASSMORPHISM TO ALL YOUR .
+That's all you have added **GLASSMORPHISM CSS** to your Web Page.
+
+# License
+
+**MIT License**
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict"
 const glasscss = (options) => { 
     options = JSON.stringify(options)
     let pieces = document.querySelectorAll(".glasscss");
@@ -5,7 +6,7 @@ const glasscss = (options) => {
     if (!options.back_ground || !(options.back_ground).length) {
         back_ground = `rgba(225,255,255, 0.25)`;        
     } else {
-        back_ground = `${options.back_ground}`; // rgba recomended with transparancy limit 0 - 0.25
+        back_ground = `${options.back_ground}`; // rgba recomended with transparancy limit 0 - 0.50
     }
 
     if (parseFloat(options.backdrop_filter) > 0 && parseFloat(options.backdrop_filter) < 20) {
@@ -20,10 +21,10 @@ const glasscss = (options) => {
         border_radius = `${options.border_radius}`;
     }
 
-    if (options.border_width === true) {
-        border_width = `1px solid rgba( 255, 255, 255, 0.18 )`;
+    if (options.border_wide === true) {
+        border_wide = `1px solid rgba( 255, 255, 255, 0.18 )`;
     } else {
-        border_width = `0px`;
+        border_wide = `0px`;
     }
     
     color_value = options.color_value
@@ -35,12 +36,65 @@ const glasscss = (options) => {
         piece.style.background = back_ground;
         piece.style.backdropFilter = backdrop_filter;
         piece.style.borderRadius = border_radius;
-        piece.style.border = border_width;
+        piece.style.border = border_wide;
         piece.style.color = color_value;
         piece.style.margin = margin_value;
         piece.style.padding = padding_value;
     })
 }
 
+
+const glassClass = (objects) => { 
+    objects = JSON.stringify(objects);
+    const classNames = Object.keys(objects);
+    const classValues = Object.values(objects);
+    classNames.forEach((className, classIndex) => {
+        let newClasses = document.querySelectorAll(`.${className}`);
+        let options = classValues[classIndex];
+        
+        if (!options.back_ground || !(options.back_ground).length) {
+            back_ground = `rgba(225,255,255, 0.25)`;        
+        } else {
+            back_ground = `${options.back_ground}`; // rgba recomended with transparancy limit 0 - 0.50
+        }
+    
+        if (parseFloat(options.backdrop_filter) > 0 && parseFloat(options.backdrop_filter) < 20) {
+            backdrop_filter = `blur(${options.backdrop_filter})`; // 0 - 20 px
+        } else {
+            backdrop_filter = `blur(4px)`;
+        }
+    
+        if (!options.border_radius || !(options.border_radius).length) {
+            border_radius = `10px`;
+        } else {
+            border_radius = `${options.border_radius}`;
+        }
+    
+        if (options.border_wide === true) {
+            border_wide = `1px solid rgba( 255, 255, 255, 0.18 )`;
+        } else {
+            border_wide = `0px`;
+        }
+        
+        color_value = options.color_value
+        margin_value = options.margin_value
+        padding_value = options.padding_value
+        
+        newClasses.forEach((newClass) => {
+            newClass.style.boxShadow = `0 8px 32px 0 rgba( 31, 38, 135, 0.37 )`;
+            newClass.style.background = back_ground;
+            newClass.style.backdropFilter = backdrop_filter;
+            newClass.style.borderRadius = border_radius;
+            newClass.style.border = border_wide;
+            newClass.style.color = color_value;
+            newClass.style.margin = margin_value;
+            newClass.style.padding = padding_value;
+        })
+    })
+}
+
 // exporting values
-module.exports.glasscss = glasscss;
+module.exports = {
+    glasscss : glasscss,
+    glassClass : glassClass
+};
