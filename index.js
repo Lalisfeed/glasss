7894ead8c8c45d36f9856e9de6bbe5566c416bbf
Lalisfeed <59479762+Lalisfeed@users.noreply.github.com>
2021-02-02 21:18:39 +0530

    change var

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
-"use strict"
-const glasscss = (options) => { 
-    options = JSON.stringify(options)
+const glasscss = (choices) => { 
+    choices = JSON.stringify(choices)
     let pieces = document.querySelectorAll(".glasscss");
     
-    if (!options.back_ground || !(options.back_ground).length) {
+    if (!choices.back_ground || !(choices.back_ground).length) {
         back_ground = `rgba(225,255,255, 0.25)`;        
     } else {
-        back_ground = `${options.back_ground}`; // rgba recomended with transparancy limit 0 - 0.50
+        back_ground = `${choices.back_ground}`; // rgba recomended with transparancy limit 0 - 0.50
     }
 
-    if (parseFloat(options.backdrop_filter) > 0 && parseFloat(options.backdrop_filter) < 20) {
-        backdrop_filter = `blur(${options.backdrop_filter})`; // 0 - 20 px
+    if (parseFloat(choices.backdrop_filter) > 0 && parseFloat(choices.backdrop_filter) < 20) {
+        backdrop_filter = `blur(${choices.backdrop_filter})`; // 0 - 20 px
     } else {
         backdrop_filter = `blur(4px)`;
     }
 
-    if (!options.border_radius || !(options.border_radius).length) {
+    if (!choices.border_radius || !(choices.border_radius).length) {
         border_radius = `10px`;
     } else {
-        border_radius = `${options.border_radius}`;
+        border_radius = `${choices.border_radius}`;
     }
 
-    if (options.border_wide === true) {
+    if (choices.border_wide === true) {
         border_wide = `1px solid rgba( 255, 255, 255, 0.18 )`;
     } else {
         border_wide = `0px`;
     }
     
-    color_value = options.color_value
-    margin_value = options.margin_value
-    padding_value = options.padding_value
+    color_value = choices.color_value
+    margin_value = choices.margin_value
+    padding_value = choices.padding_value
     
     pieces.forEach((piece) => {
         piece.style.boxShadow = `0 8px 32px 0 rgba( 31, 38, 135, 0.37 )`;
@@ -49,8 +48,8 @@ const glassClass = (objects) => {
     const classNames = Object.keys(objects);
     const classValues = Object.values(objects);
     classNames.forEach((className, classIndex) => {
-        let newClasses = document.querySelectorAll(`.${className}`);
-        let options = classValues[classIndex];
+        let newClasses = document.querySelectorAll("." + className);
+        options = classValues[classIndex];
         
         if (!options.back_ground || !(options.back_ground).length) {
             back_ground = `rgba(225,255,255, 0.25)`;        
